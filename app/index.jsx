42ebd7feef17af841a20d0fb7e9d2c2cc0e7f134
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-12 01:06:06 +0530

    bugfix [nativewind > tailwindcss] : fix nativewind error

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-3xl">Hello world! 1</Text>
+            <Text className="text-3xl">Hello world! </Text>
             <Text>&</Text>
             <Text>We are live!</Text>
             <StatusBar style="auto" />
