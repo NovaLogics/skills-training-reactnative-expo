03d9084ece29c18e6ae1c2af77ce2fe035dcbe00
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-11 21:49:27 +0530

    build [ nativewind ] : [stage 2] Install Nativewind

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>Hello world!</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-3xl">Hello world! 1</Text>
             <Text>&</Text>
             <Text>We are live!</Text>
             <StatusBar style="auto" />
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
