6bdb5dca0444fe9ad5c26817a32c4c573a95216e
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-12 18:27:30 +0530

    feat [ custom-button ] : [stage 5] Basic impl custom component  > button

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -1,11 +1,39 @@
 import { TouchableOpacity, Text } from 'react-native';
 import React from 'react';
 
-const CustomButton = () => {
+const CustomButton = ({
+    title,
+    handlePress,
+    containerStyles,
+    textStyles,
+    isLoading
+}) => {
     return (
         <TouchableOpacity
-            className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center`}>
-            <Text >Button</Text>
+            className={`
+                bg-secondary 
+                rounded-3xl
+                min-h-[62px] 
+                justify-center 
+                items-center
+                 ${containerStyles}
+                 ${isLoading ? "opacity-50" : ""}
+                 `}
+            onPress={handlePress}
+            activeOpacity={0.7}
+            disabled={isLoading}
+        >
+
+            <Text
+                className={`
+                    text-primary 
+                    font-psemibold 
+                    text-lg
+                    ${textStyles}
+                `}>
+                {title}
+            </Text>
+
         </TouchableOpacity>
     );
 };
