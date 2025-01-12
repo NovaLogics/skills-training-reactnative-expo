28669d88c9689b0980b683c466efb679a24b5713
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-12 20:58:09 +0530

    feat [ sign-in ] : [stage 6] Import FormField to SignIn

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,10 +1,17 @@
 import { View, Text, ScrollView, Image } from 'react-native'
-import React from 'react'
+import {React, useState} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from '../../constants'
+import { FormField } from '../../components'
 
 const SignIn = () => {
+
+  const [form, setForm] = useState({
+    email: "",
+    password: ""
+  })
+
   return (
     <SafeAreaView
       className="bg-primary h-full">
@@ -15,10 +22,32 @@ const SignIn = () => {
             source={images.logo}
             resizeMode="contain"
             className="w-[115px] h-[35px]" />
+
           <Text
             className="text-2xl text-white text-semibold mt-10 font-psemibold">
             Login to VidBox
           </Text>
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(event) => setForm({
+              ...form,
+              email: event
+            })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(event) => setForm({
+              ...form,
+              password: event
+            })}
+            otherStyles="mt-7"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
