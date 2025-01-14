b08074c45d49155826277508b3736e950da829b4
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-14 18:54:37 +0530

    feat [ home-ui ] : [stage 9] Update EmptyState Ui component

--- components/EmptyState.jsx
+++ components/EmptyState.jsx
@@ -2,6 +2,8 @@ import { View, Text, Image } from 'react-native'
 import React from 'react'
 
 import { images } from '../constants'
+import CustomButton from './CustomButton'
+import { router } from 'expo-router'
 
 const EmptyState = ({
     title,
@@ -22,6 +24,12 @@ const EmptyState = ({
                 className="font-pmedium text-sm text-gray-100">
                 {subtitle}
             </Text>
+
+            <CustomButton
+            title="Create video"
+            handlePress={() => router.push("/create")}
+            containerStyles="w-full my-5"
+            />
         </View>
     )
 }
