f57eafebab08a843531c505f842a440e74835373
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-16 20:17:46 +0530

    refactor [ component-form-field ] : Update FormField Component

--- shared/components/FormField.jsx
+++ shared/components/FormField.jsx
@@ -1,7 +1,6 @@
-import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
-import { React, useState } from 'react'
-
-import { icons } from '../constants'
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { icons } from '../constants';
 
 const FormField = ({
     title,
@@ -10,66 +9,41 @@ const FormField = ({
     handleChangeText,
     otherStyles,
     keyboardType,
+    isPasswordField = false,
     ...props
 }) => {
-
-    const [showPassword, setShowPassword] = useState(false)
+    const [showPassword, setShowPassword] = useState(false);
 
     return (
-        <View
-            className={`
-                space-y-2
-                ${otherStyles}
-                `}>
-            <Text
-                className="
-                    text-base 
-                    text-gray-100 
-                    font-pmedium
-                ">
+        <View className={`space-y-2 ${otherStyles}`}>
+            {/* Title text */}
+            <Text className="text-base text-gray-100 font-pmedium">
                 {title}
             </Text>
-            <View
-                className="
-                    border-2
-                    border-gray-100
-                    w-full
-                    h-16
-                    px-4
-                    bg-black-100
-                    rounded-3xl 
-                    focus:border-secondary
-                    items-center
-                    flex-row
-                ">
+            {/* Input field container */}
+            <View className="border-2 border-gray-100 w-full h-16 px-4 bg-black-100 rounded-3xl focus:border-secondary items-center flex-row mt-2">
+                {/* TextInput View*/}
                 <TextInput
-                    className="
-                    flex-1
-                    text-white
-                    font-psemibold
-                    text-base
-                    "
+                    className="flex-1 text-white font-psemibold text-base"
                     value={value}
                     placeholder={placeholder}
                     placeholderTextColor="#7b7b8b"
                     onChangeText={handleChangeText}
-                    secureTextEntry={title === "Password" && !showPassword}
+                    secureTextEntry={isPasswordField && !showPassword}
                 />
-
-                {title === "Password" && (
-                    <TouchableOpacity
-                        onPress={() => setShowPassword(!showPassword)}>
+                {/* Toggle visibility of password */}
+                {isPasswordField && (
+                    <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
                         <Image
                             source={!showPassword ? icons.eye : icons.eyeHide}
                             className="w-6 h-6"
-                            resizeMode="contain" />
-
+                            resizeMode="contain"
+                        />
                     </TouchableOpacity>
                 )}
-
             </View>
         </View>
-    )
-}
+    );
+};
 
-export default FormField
+export default FormField;
