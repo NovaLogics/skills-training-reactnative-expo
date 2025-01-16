03a8a8d07432ba74662530c555f69b637451ec8c
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-16 23:20:25 +0530

    refactor [ component-info-box ] : Update InfoBox component

--- shared/components/InfoBox.jsx
+++ shared/components/InfoBox.jsx
@@ -1,25 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text } from 'react-native';
 
 const InfoBox = ({
-    title,
-    subtitle,
-    containerStyles,
-    titleStyles,
+    title = "",
+    subtitle = "",
+    containerStyles = "",
+    titleStyles = "",
 }) => {
     return (
         <View
             className={containerStyles}>
+            {/* Title */}
             <Text
-                className={`text-white text-center font-psemibold ${titleStyles}`}>
+                className={`text-white text-center font-psemibold ${titleStyles}`}
+                accessibilityRole="header">
                 {title}
             </Text>
+            {/* Subtitle */}
             <Text
-                className={"text-sm text-gray-100 text-center font-pregular"}>
+                className="text-sm text-gray-100 text-center font-pregular"
+                accessibilityRole="text">
                 {subtitle}
             </Text>
         </View>
-    )
-}
+    );
+};
 
-export default InfoBox
+export default InfoBox;
