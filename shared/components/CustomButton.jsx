2db268499e8a3ba727900fa1923addaacdfc673c
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-16 23:17:27 +0530

    refactor [ component-button ] : Update custom button component

--- shared/components/CustomButton.jsx
+++ shared/components/CustomButton.jsx
@@ -9,17 +9,16 @@ const CustomButton = ({
     isLoading
 }) => {
     return (
-        <View className="flex-1 w-full justify-center items-center m-0">
+        <View 
+        className="flex-1 w-full justify-center items-center m-0">
         <TouchableOpacity
             className={`bg-secondary-200 rounded-full min-h-[54px] justify-center items-center ${containerStyles} ${isLoading ? "opacity-50" : ""}`}
             onPress={handlePress}
             activeOpacity={0.7}
-            disabled={isLoading}
-        >
+            disabled={isLoading}>
             {/* Title text */}
             <Text
-                className={`text-primary font-psemibold text-xl ${textStyles}`}
-            >
+                className={`text-primary font-psemibold text-xl ${textStyles}`}>
                 {title}
             </Text>
         </TouchableOpacity>
