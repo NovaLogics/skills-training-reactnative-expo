732094dee415d1831bcdfe547f5b59425bb9e994
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-16 23:23:47 +0530

    refactor [ component-video-card ] : Update VideoCard component

--- shared/components/VideoCard.jsx
+++ shared/components/VideoCard.jsx
@@ -1,12 +1,9 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import { React, useState, } from 'react'
-import { icons } from '../constants'
-import { ResizeMode, Video } from "expo-av";
-import { Dimensions } from "react-native";
-import FullScreenVideo from './FullScreenVideo';
-import { Redirect, router } from 'expo-router';
+import { Dimensions } from 'react-native';
+import { React, useState } from 'react';
+import { ResizeMode, Video } from 'expo-av';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 
-const { width, height } = Dimensions.get("window");
+import { icons } from '../constants';
 
 const VideoCard = ({
     video: {
@@ -15,45 +12,52 @@ const VideoCard = ({
         video,
         creator: {
             username,
-            avatar,
+            avatar
         }
     }
 }) => {
-
+    const { width } = Dimensions.get('window');
     const [play, setPlay] = useState(false);
 
     return (
         <View
             className="flex-col items-center px-4 mb-14">
+            {/* Divider */}
+            <View
+                style={{
+                    width: '100%',
+                    height: 1,
+                    backgroundColor: '#232533',
+                    marginBottom: 16,
+                }}
+            />
+            {/* Creator information */}
             <View
                 className="flex-row gap-3 items-start">
                 <View
                     className="justify-center items-center flex-row flex-1">
-                    <View
-                        className="w-[46px] h-[46px] rounded-lg border border-secondary justify-center items-center p-0.5">
+                    {/* User Avatar */}
+                    <View className="w-[46px] h-[46px] rounded-full border border-2 border-secondary-200 justify-center items-center ">
                         <Image
                             source={{ uri: avatar }}
-                            className="w-full h-full rounded-lg"
+                            className="w-full h-full rounded-full"
                             resizeMode="cover"
                         />
                     </View>
                     <View
                         className="justify-center flex-1 ml-3 gap-y-1">
                         <Text
-                            className="text-white text-sm font-psemibold"
-                            numberOfLines={1}>
+                            className="text-white text-sm font-psemibold" numberOfLines={1}>
                             {title}
                         </Text>
                         <Text
-                            className="text-gray-100 text-xs font-pregular"
-                            numberOfLines={1}>
+                            className="text-gray-100 text-xs font-pregular" numberOfLines={1}>
                             {username}
                         </Text>
                     </View>
                 </View>
-
-                <View
-                    className="pt-2">
+                {/* Menu icon */}
+                <View className="pt-2">
                     <Image
                         source={icons.menu}
                         className="w-5 h-5"
@@ -62,13 +66,15 @@ const VideoCard = ({
                 </View>
             </View>
 
+            {/* Video || thumbnail*/}
             {play ? (
+                // Video player when play is true
                 <Video
                     source={{ uri: video }}
                     className="w-full h-60 rounded-xl mt-3 bg-white/10"
                     style={{
-                        width: width, 
-                        height: 280, 
+                        width: width,
+                        height: 280,
                         marginTop: 3,
                         backgroundColor: "rgba(255, 255, 255, 0.1)",
                     }}
@@ -81,29 +87,29 @@ const VideoCard = ({
                         }
                     }}
                 />
-              //  <FullScreenVideo videoUri={{video}} />
-            ) :
-                (
-                    <TouchableOpacity
-                        activeOpacity={0.7}
-                        onPress={() => setPlay(true)}
-                        className="w-full h-60 rounded-xl mt-3 relative justify-center items-center">
-                        <Image
-                            source={{ uri: thumbnail }}
-                            className="w-full h-full rounded-xl mt-3"
-                            resizeMode="cover"
-                        />
-                        <Image
-                            source={icons.play}
-                            className="w-12 h-12 absolute"
-                            resizeMode="contain"
-                        />
+                //  <FullScreenVideo videoUri={video} />
+            ) : (
+                // Thumbnail view when video is not playing
+                <TouchableOpacity
+                    activeOpacity={0.7}
+                    onPress={() => setPlay(true)}
+                    className="w-full h-60 rounded-xl mt-3 relative justify-center items-center"
+                >
+                    <Image
+                        source={{ uri: thumbnail }}
+                        className="w-full h-full rounded-xl mt-3"
+                        resizeMode="cover"
+                    />
+                    <Image
+                        source={icons.play}
+                        className="w-12 h-12 absolute"
+                        resizeMode="contain"
+                    />
+                </TouchableOpacity>
+            )}
+        </View >
 
-                    </TouchableOpacity>
-                )}
+    );
+};
 
-        </View>
-    )
-}
-
-export default VideoCard
+export default VideoCard;
