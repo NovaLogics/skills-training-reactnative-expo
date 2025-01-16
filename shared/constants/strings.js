68066decdef4a919a91fa33629457f4f197428df
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-16 23:02:52 +0530

    build [ constants ] : Add strings res

--- shared/constants/strings.js
+++ shared/constants/strings.js
@@ -1,6 +1,7 @@
 export const messages = {
     ERROR: "Error",
     SUCCESS: "Success",
+    INCOMPLETE_FIELDS: "Incomplete Fields",
     SIGN_IN_SUCCESS: "User signed in successfully",
     FILL_FIELDS: "Please fill in all the fields",
     POST_SUCCESS: "Post Uploaded Successfully",
@@ -10,4 +11,10 @@ export const routes = {
     HOME: "/home",
     SIGN_IN: "/sign-in",
     SIGN_UP: "/sign-up",
+};
+
+
+export const mediaType = {
+    VIDEOS: "videos",
+    IMAGES: "images",
 };
