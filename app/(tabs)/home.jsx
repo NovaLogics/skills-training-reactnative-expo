0458051de2342e2e9058ab95460065ac85c4af14
NovaLogics <rajitha.shavinda@gmail.com>
2025-01-16 23:12:49 +0530

    refactor [ feature-home-screen ] : Update home screen

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,29 +1,24 @@
-import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native'
-import { React, useState, useEffect } from 'react'
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { React, useState } from 'react';
+import { View, Text, FlatList, Image, RefreshControl } from 'react-native';
 
-import { images } from '../../shared/constants'
-import { EmptyState, SearchInput, Trending, VideoCard } from "../../shared/components";
-import { getAllPosts, getLatestPosts } from '../../shared/api/appwrite'
-import useAppWrite from '../../shared/api/useAppWrite'
+import { images } from '../../shared/constants';
+import useAppwrite from '../../shared/api/useAppwrite';
 import { useGlobalContext } from "../../shared/context/GlobalProvider";
-
+import { getAllPosts, getLatestPosts } from '../../shared/api/appwrite';
+import { EmptyState, SearchInput, Trending, VideoCard } from "../../shared/components";
 
 const Home = () => {
-  const [refreshing, setRefreshing] = useState(false)
-
-  const { data: posts, refetch } = useAppWrite(getAllPosts)
-
-  const { data: latestPosts } = useAppWrite(getLatestPosts)
-
-    const { user } = useGlobalContext();
+  const { user } = useGlobalContext();
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const [refreshing, setRefreshing] = useState(false);
 
   const onRefresh = async () => {
-    setRefreshing(true)
-    await refetch()
-    setRefreshing(false)
-  }
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
 
   return (
     <SafeAreaView
@@ -31,9 +26,10 @@ const Home = () => {
       <FlatList
         data={posts}
         keyExtractor={(item) => item.$id}
+        // Header component
         ListHeaderComponent={() => (
-          <View
-            className="my-6 px-4 space-y-6">
+          <View className="my-6 px-4 space-y-6">
+            {/* Welcome message */}
             <View
               className="justify-between items-start flex-row mb-6">
               <View>
@@ -46,54 +42,47 @@ const Home = () => {
                   {user.username ?? "User"}
                 </Text>
               </View>
-
+              {/* Logo */}
               <View
                 className="mt-1.5">
                 <Image
                   source={images.logoSmall}
                   className="w-9 h-10"
-                  resizeMode="contain" />
-
+                  resizeMode="contain"
+                />
               </View>
             </View>
-
+            {/* Search input */}
             <SearchInput />
-
+            {/* Latest videos */}
             <View
               className="w-full flex-1 pt-5 pb-8">
               <Text
                 className="text-gray-100 text-lg font-pregular mb-3">
                 Latest Videos
               </Text>
-
               <Trending
                 posts={latestPosts ?? []}
               />
-
             </View>
-
           </View>
         )}
-        renderItem={({ item }) => (
-          <VideoCard
-            video={item}
-          />
-        )}
+        // Render video card
+        renderItem={({ item }) => <VideoCard video={item} />}
+        // Empty state
         ListEmptyComponent={() => (
           <EmptyState
             title="No Videos Found"
             subtitle="Be the first one to upload a video"
           />
         )}
+        // Refresh control
         refreshControl={
-          <RefreshControl
-            refreshing={refreshing}
-            onRefresh={onRefresh}
-          />
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
         }
       />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
